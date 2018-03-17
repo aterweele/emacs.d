511a06ebd76a8fe3367d8507a3ce85f271aa2ff7
Alex ter Weele <alex.ter.weele@gmail.com>
2018-03-17 04:08:43 -0500

    Global keybindings.
    C-c o for occur, because M-s o is shadowed in paredit.
    
    magit-status deserves a bind of its own.

--- init.el
+++ init.el
@@ -35,6 +35,7 @@
 (require 'helm-config)
 (helm-mode 1)
 (define-key global-map [remap find-file] 'helm-find-files)
+(global-set-key (kbd "C-c o") 'occur)
 (define-key global-map [remap occur] 'helm-occur)
 (define-key global-map [remap list-buffers] 'helm-buffers-list)
 (global-set-key (kbd "M-x") 'helm-M-x)
